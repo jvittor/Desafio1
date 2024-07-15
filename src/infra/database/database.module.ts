0b32c6f4c04306df55ac08c4bedefbd75b20fa5e
jvittor <102699222+jvittor@users.noreply.github.com>
2024-07-15 20:06:06 -0300

    :sparkles: feat: add import Note Repository in database module

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { UserRepository } from 'src/modules/user/repositories/UserRepository';
+import { NoteRepository } from 'src/modules/notes/repositories/NoteRepository';
+import { PrismaNoteRepository } from './prisma/repositories/notes/PrismaNoteRepository';
 import { PrismaUserRepository } from './prisma/repositories/user/PrismaUserRepository';
 
 @Module({
@@ -10,7 +12,11 @@ import { PrismaUserRepository } from './prisma/repositories/user/PrismaUserRepos
       provide: UserRepository,
       useClass: PrismaUserRepository,
     },
+    {
+      provide: NoteRepository,
+      useClass: PrismaNoteRepository,
+    },
   ],
-  exports: [UserRepository],
+  exports: [UserRepository, NoteRepository],
 })
 export class DatabaseModule {}
