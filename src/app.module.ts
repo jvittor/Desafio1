5cb8f560640fd92ea365958912ade8dfe5c5866d
jvittor <102699222+jvittor@users.noreply.github.com>
2024-07-15 20:05:28 -0300

    :sparkles: feat: add import notes module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,17 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './infra/http/modules/user/user.module';
 import { DatabaseModule } from './infra/database/database.module';
 import { AuthModule } from './infra/http/modules/auth/auth.module';
+import { NotesModule } from './infra/http/modules/notes/notes.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './infra/http/modules/auth/guards/jwtAuth.guard';
 
-import { MailerModule } from '@nestjs-modules/mailer';
-import { env } from './env';
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule],
+  imports: [DatabaseModule, UserModule, NotesModule, AuthModule],
   controllers: [],
   providers: [
     {
       provide: APP_GUARD,
-      useClass: JwtAuthGuard,
+      useClass: JwtAuthGuard
     },
   ],
 })
